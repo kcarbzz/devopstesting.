56047f624b80a898fcc17f9b20bb5a23f6220edc
kcarbzz <rakion2r@gmail.com>
2025-11-24 19:40:49 -0500

    feat/primerosCambios

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          DevOps Testing  <code>CI-CD</code> ESAN Project
+        </p>
+
+          Learn more on React CI/CD Pipeline
+          <a href="https://www.esan.edu.pe/">learn react</a>
+          <p>Prueba de SOftware</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
